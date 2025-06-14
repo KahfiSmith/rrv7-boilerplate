d413afc038830a66628cdc76f49aa8dddf82d6fd
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2025-06-14 12:54:17 +0700

    refactor: update route configuration

--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,27 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-const createRoute = (path: string) => route(path, `routes/${path}/index.tsx`);
-const createAuthRoute = (path: string) =>
-  route(`auth/${path}`, `routes/auth/${path}/index.tsx`);
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/_index.tsx"),
-
-//   createAuthRoute("login"),
-//   createAuthRoute("register"),
-//   createAuthRoute("forgot-password"),
-//   createAuthRoute("reset-password"),
-
-//   createRoute("dashboard"),
+  
+  // ...prefix("auth", [
+  //   layout("routes/auth/_layout.tsx", [
+  //     route("login", "routes/auth/login.tsx"),
+  //     route("register", "routes/auth/register.tsx"),
+  //     route("forgot-password", "routes/auth/forgot-password.tsx"),
+  //     route("reset-password", "routes/auth/reset-password.tsx"),
+  //   ]),
+  // ]),
+  
+  // ...prefix("dashboard", [
+  //   index("routes/dashboard/index.tsx"),
+  //   route("profile", "routes/dashboard/profile.tsx"),
+  //   route("settings", "routes/dashboard/settings.tsx"),
+    
+  //   layout("routes/dashboard/users/_layout.tsx", [
+  //     route("users", "routes/dashboard/users/index.tsx"),
+  //     route("users/:id", "routes/dashboard/users/detail.tsx"),
+  //   ]),
+  // ]),
+  
+  route("*", "routes/not-found.tsx"),
 ] satisfies RouteConfig;
