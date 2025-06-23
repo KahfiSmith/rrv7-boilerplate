d6763d11ff47e5988ba03d9e026f6e4ba70292be
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2025-06-23 14:09:02 +0700

    chore: update ESLint configuration to include global variables and refine TypeScript rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import react from 'eslint-plugin-react';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   {
@@ -24,6 +24,24 @@ export default [
       parser: tsParser,
       ecmaVersion: 2022,
       sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: 'readonly',
+        JSX: 'readonly',
+        NodeJS: 'readonly',
+        HTMLElement: 'readonly',
+        HTMLDivElement: 'readonly',
+        HTMLButtonElement: 'readonly',
+        HTMLInputElement: 'readonly',
+        HTMLParagraphElement: 'readonly',
+        HTMLHeadingElement: 'readonly',
+        RequestInit: 'readonly',
+        FormData: 'readonly',
+        File: 'readonly',
+        StorageEvent: 'readonly',
+        CustomEvent: 'readonly'
+      },
       parserOptions: {
         ecmaFeatures: {
           jsx: true
@@ -33,43 +51,29 @@ export default [
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       'jsx-a11y': jsxA11y,
       '@typescript-eslint': tseslint
     },
-    rules: {
-      // TypeScript rules
+    rules: {    
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/prefer-const': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
       
-      // React rules
-      'react/jsx-uses-react': 'off', // Not needed in React 17+
-      'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+      'react/jsx-uses-react': 'off', 
+      'react/react-in-jsx-scope': 'off', 
       'react/jsx-uses-vars': 'error',
-      'react/prop-types': 'off', // Using TypeScript instead
+      'react/prop-types': 'off', 
       'react/display-name': 'warn',
       
-      // React Hooks rules
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       
-      // React Refresh rules
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-      
-      // Accessibility rules
       'jsx-a11y/alt-text': 'error',
       'jsx-a11y/anchor-has-content': 'error',
-      'jsx-a11y/anchor-is-valid': 'error',
-      'jsx-a11y/click-events-have-key-events': 'warn',
+      'jsx-a11y/anchor-is-valid': 'error',      'jsx-a11y/click-events-have-key-events': 'warn',
       'jsx-a11y/no-static-element-interactions': 'warn',
       
-      // General rules
-      'no-unused-vars': 'off', // Using TypeScript version instead
-      'no-console': 'warn',
+      'no-unused-vars': 'off', 
+      'no-console': 'off',
       'prefer-const': 'error',
       'no-var': 'error'
     },
